b0fbdba9e4f1c6f44d55e25bd2d4e2c9fe896c8b
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-07-06 20:29:19 +0100

    task 100

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from typing import Dict, Any, TypeVar
+
+K = TypeVar('K')
+V = TypeVar('V')
+
+
+def safely_get_value(dct: Dict[K, V], key: K, default: V = None) -> V:
+    if key in dct:
+        return dct[key]
+    else:
+        return default
