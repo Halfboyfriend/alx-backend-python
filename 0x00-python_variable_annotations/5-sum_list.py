04b17bcc5233c3aee75dbd166ba8b35521bf0c86
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-07-06 17:57:33 +0100

    task5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,6 +1,3 @@
 #!/usr/bin/env python3
-def sum_list (input_list: float) -> float:
-    count = 0
-    for i in input_list:
-        count += i
-    return count
+def sum_list (input_list: list(float)) -> float:
+    return sum(input_list)
