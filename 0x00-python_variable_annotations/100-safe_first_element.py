2ba882fa938a47802009d02e50c2868016d8862d
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-07-06 20:26:48 +0100

    task 100

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
-print('Hello')
+from typing import Optional, Any
+
+
+def safe_first_element(lst: list) -> Optional[Any]:
+    if lst:
+        return lst[0]
+    else:
+        return None
