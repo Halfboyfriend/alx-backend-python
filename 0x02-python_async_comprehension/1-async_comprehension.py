ec1cb339c1cf43f53720e518179c3ce0a7f5b53a
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-07-13 18:15:31 +0100

    ADVANCED ASYNC

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+TASK 1"""
+from importlib import import_module as using
+from typing import List
+
+
+async_generator = using('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Import async_generator from the previous task and then write a 
+    coroutine called async_comprehension that takes no arguments."""
+    return [num async for num in async_generator()]
